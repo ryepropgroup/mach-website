d1b0822d0d5855fccd5c337c5d1251259f7c642b
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-09-20 17:19:48 -0700

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,78 @@
+import {Box, Center, Link, LinkProps, Text, TextProps} from "@chakra-ui/react";
+
+const initialStyle: TextProps = {
+  as: "span",
+  color: "white",
+  fontSize: "6xl",
+  fontWeight: 700,
+};
+
+const acronymStyle: TextProps = {
+  as: "span",
+  color: "gray.400",
+  fontSize: "5xl",
+  fontWeight: 700,
+};
+
+const normalStyle: TextProps = {
+  as: "span",
+  color: "white",
+  fontSize: "4xl",
+};
+
+const linkStyle: LinkProps = {
+  color: "purple.400",
+  fontSize: "4xl",
+};
+
+const lreStyle: TextProps = {
+  as: "span",
+  color: "teal.300",
+  fontSize: "4xl",
+  fontWeight: 700,
+};
+
 export default function Index() {
   return (
-    <>
-      <h1>MACH</h1>
-    </>
+    <Box h="100vh" bg="black">
+      <Center>
+        <Box>
+          <Text {...initialStyle}>M</Text>
+          <Text {...acronymStyle}>etropolitan</Text>
+          <br />
+          <Text {...initialStyle}>A</Text>
+          <Text {...acronymStyle}>erospace</Text>
+          <Text {...initialStyle} fontSize="5xl">
+            {" &"}
+          </Text>
+          <br />
+          <Text {...initialStyle}>C</Text>
+          <Text {...acronymStyle}>ombustion</Text>
+          <br />
+          <Text {...initialStyle}>H</Text>
+          <Text {...acronymStyle}>ub</Text>
+        </Box>
+      </Center>
+      <Center>
+        <Box>
+          <Text {...normalStyle}>is {"a "}</Text>
+          <Text {...normalStyle} fontWeight={700}>
+            student design team
+          </Text>
+          <Text {...normalStyle}>{" from "}</Text>
+          <Link {...linkStyle} href="https://torontomu.ca" isExternal>
+            Toronto Metropolitan University
+          </Link>
+          <Text {...normalStyle}>,</Text>
+        </Box>
+      </Center>
+      <Center>
+        <Box>
+          <Text {...normalStyle}>building a {"bipropellant "}</Text>
+          <Text {...lreStyle}>liquid rocket engine</Text>
+          <Text {...normalStyle}>.</Text>
+        </Box>
+      </Center>
+    </Box>
   );
 }
