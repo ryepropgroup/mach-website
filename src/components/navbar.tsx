43fd1f9c1340e2e4fd07f35cf3ddfa0d18113521
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-09-20 21:43:52 -0700

    style: reformat navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,41 +1,63 @@
-import {HamburgerIcon}   from "@chakra-ui/icons";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import {
-  Box, Button,
+  Box,
+  Button,
   ButtonGroup,
   Container,
   Flex,
-  HStack, IconButton, Image,
-  Link, Text,
+  HStack,
+  IconButton,
+  Image,
+  Link,
+  Text,
   useColorModeValue,
 } from "@chakra-ui/react";
 
-export default function Navbar(): JSX.Element {
-  const isDesktop = true;//useBreakpointValue({base: false, lg: true})
+export default function Navbar() {
+  const isDesktop = true; // useBreakpointValue({base: false, lg: true})
   return (
-    <Flex as="section" pos="static" align="flex-start" pb={{base: '12', md: '24'}} pl="10">
-      <Box as="nav" bg="bg-surface" shadow={useColorModeValue('sm', 'sm-dark')}>
-        <Container py={{base: '4', lg: '5'}}>
+    <Flex
+      as="section"
+      pos="static"
+      align="flex-start"
+      pb={{ base: "12", md: "24" }}
+      pl="10"
+    >
+      <Box as="nav" bg="bg-surface" shadow={useColorModeValue("sm", "sm-dark")}>
+        <Container py={{ base: "4", lg: "5" }}>
           <HStack justify="space-between" spacing="10">
             <Image alt="mach logo" src="/img/mach_logo.png"></Image>
             {isDesktop ? (
               <Flex justify="space-between" flex="1" pl="10">
                 <ButtonGroup spacing="8" variant="link">
-                  {['About','Engine', 'Team', 'Sponsors','Contact'].map(( item ) => (
-                    <NavItem key={item} item={item}></NavItem>
-                  ))}
+                  {["About", "Engine", "Team", "Sponsors", "Contact"].map(
+                    (item) => (
+                      <NavItem key={item} item={item}></NavItem>
+                    )
+                  )}
                 </ButtonGroup>
               </Flex>
             ) : (
-               <IconButton aria-label="Open Menu" icon={<HamburgerIcon/>} variant="ghost"/>
-             )}
+              <IconButton
+                aria-label="Open Menu"
+                icon={<HamburgerIcon />}
+                variant="ghost"
+              />
+            )}
           </HStack>
         </Container>
       </Box>
     </Flex>
   );
 }
-const NavItem = ({item}:{item:string}):JSX.Element=>{
-  return(
-    <Button variant="link"><Link href={`/${item.toLowerCase()}`}><Text color="gray.200" fontSize="3xl">{item}</Text></Link></Button>
-  )
-}
+const NavItem = ({ item }: { item: string }) => {
+  return (
+    <Button variant="link">
+      <Link href={`/${item.toLowerCase()}`}>
+        <Text color="gray.200" fontSize="3xl">
+          {item}
+        </Text>
+      </Link>
+    </Button>
+  );
+};
