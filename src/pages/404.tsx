390720ca82ee05e127f2953af7da6a5a8a437a70
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-09-30 22:57:11 -0700

    feat: dark mode 404 and 500 pages
    Fixes #19.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,24 @@
+import { Box, Button, Center, Heading, Link } from "@chakra-ui/react";
+
+import Header from "@/config";
+import { bgStyle, h1Style } from "@/styles";
+
 export default function Error() {
   return (
     <>
-      <h1>404 - Something went wrong</h1>
+      <Header title="MACH :: 404" />
+      <Box {...bgStyle} bg="black" />
+      <Center h="100vh">
+        <Box>
+          <Heading {...h1Style}>404 - Not Found</Heading>
+          <br />
+          <Center>
+            <Link href="/mach-website">
+              <Button size="lg">Return to Home Page</Button>
+            </Link>
+          </Center>
+        </Box>
+      </Center>
     </>
   );
 }
