2e7d2ad5709e5f8ceb1ce56911f6e2d7cb6215f2
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-10-04 12:59:11 -0700

    fix: add email copy to socials footer

--- src/components/socialsFooter.tsx
+++ src/components/socialsFooter.tsx
@@ -1,9 +1,34 @@
-import { Center, IconButton, Link } from "@chakra-ui/react";
-import { AiOutlineInstagram, AiOutlineLinkedin } from "react-icons/ai";
+import {
+  Center,
+  IconButton,
+  Link,
+  Tooltip,
+  useClipboard,
+} from "@chakra-ui/react";
+import {
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+  AiOutlineMail,
+} from "react-icons/ai";
 
 export default function SocialsFooter() {
+  const { hasCopied, onCopy } = useClipboard("mach@ryerson.ca");
   return (
-    <Center m="2%">
+    <Center as="footer" m="2%">
+      <Tooltip
+        closeOnClick={false}
+        hasArrow
+        label={hasCopied ? "Email Copied!" : "Copy Email"}
+      >
+        <IconButton
+          mx="0.5%"
+          color="white"
+          bg="black"
+          aria-label="MACH Email"
+          icon={<AiOutlineMail size="50px" />}
+          onClick={onCopy}
+        />
+      </Tooltip>
       <Link mx="0.5%" href="https://www.instagram.com/mach_tmu/">
         <IconButton
           color="white"
